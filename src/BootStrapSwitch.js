c32e402bba615164faed3612d7f49c55d8faa9dd
sansetty2510 <sansetty25@gmail.com>
2017-03-16 15:43:48 +0530

    Update BootStrapSwitch.js

--- src/BootStrapSwitch.js
+++ src/BootStrapSwitch.js
@@ -1,27 +1,35 @@
 import React from 'react';
 import {render} from 'react-dom';
-import SwitchButton from 'react-switch-button';
+import Toggle from './react-bootstrap-toggle';
 
 export default class BootStrapSwitch extends React.Component {
 
-	constructor(props){
-		super(props);
-		this.state = {
-			baconIsReady : true
-		};
-		this.toggle = this.toggle.bind(this);
+	constructor() {
+	    super();
+	    this.onClick = this.onClick.bind(this);
+	    this.state = { active: true, yes: <span>Yes</span> };
 	}
 
-	toggle(event) {
-	  console.log("inside handleChange");
+	onClick() {
+		this.setState({ active: !this.state.active, yes: <span>Yss</span> });
 	}
 
-	render(){
-		return(
-			<div>
-				<h2> BootStrapSwitch </h2>
-				<SwitchButton name="switch-1" />
-			</div>
-		)
-	};
+	render() {
+	    const style = {
+	      margin: '20px 20px',
+	    };
+
+	    return (
+	      <div style={style}>
+	        <Toggle
+	          active={this.state.active}
+	          on={this.state.yes}
+	          off="OFF"
+	          handlestyle="warning"
+	          onClick={this.onClick}
+	          size="lg"
+	        />
+	      </div>
+	    );
+	}
 }
